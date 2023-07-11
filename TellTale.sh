b774f96dd3fe4da13440aa9b9c65cf940c02dd75
SYN-3r <135239201+SYN-3r@users.noreply.github.com>
2023-07-11 14:56:14 +0000

    Update TellTale.sh

--- TellTale.sh
+++ TellTale.sh
@@ -7,13 +7,14 @@ C=$(printf '\033')
 Magenta="${C}[1;95m"
 LGray="${C}[1;37m"
 DGray="${C}[1;90m"
-Blue="${C}[1;34m&${C}[0m"
+Blue="${C}[1;34m"
 Cyan="${C}[1;96m"
 Red="${C}[1;31m"
+Normal="${C}[0m"
 
 
 printf """
-${LGray}/-------------------------------------------------------------------------\
+${LGray}/-------------------------------------------------------------------------\\
 |                         ${Magenta}♥  TellTale by SYN-3r  ♥${LGray}                        |
 |                       ${Magenta}A Linux Enumeration Script${LGray}                        |
 |-------------------------------------------------------------------------|
@@ -46,20 +47,33 @@ ${LGray}/-----------------------------------------------------------------------
 #                             USERS
 #_________________________________________________________________
 
+#basic start up info for log on
 #View what users are on the machine
-loggedon=$w
+loggedon=$( w | awk -F" " '{ print $1,$4 }')
 echo "Users Currently on the machine:\n $loggedon"
 
 #View which user you are
-$user=whoami
+user=$whoami
 echo "You are currently: $user"
 
-#Present Menu
-until [ "$selection" = "0" ]; do
-        echo "What would you like to do?\n 1. Find Sudo Users\n 2.See Accesses"
-        read 
-        if $selection == 1;
-                #Find sudo users
+#MENU display start
+while true; 
+do
+        printf """
+        ${Blue}What would you like to do?
+        __________________________
+        
+        ${Magenta}1. Find Users
+        ${Magenta}2. See what $user can do
+        ${Magenta}3. See Accesses
+        ${Red}Q. Quit${Normal}
+        
+        """
+        read -r selection
+
+        #Find Users
+        if [ $selection == "1" ];
+        then
                 #check to see if root already
                 if ([ -f /usr/bin/id ] && [ "$(/usr/bin/id -u)" -eq "0" ]) || [ "`whoami 2>/dev/null`" = "root" ];
                 then
@@ -67,16 +81,17 @@ until [ "$selection" = "0" ]; do
                 else
                         echo "You are not root. Searching for sudo users now..."
         
-                #search for sudo iusers
-                Sudo1=$(awk -F: '($3 == "0") {print}' /etc/passwd 2>/dev/null
-                Sudo2=$(grep -v -E "^#" /etc/passwd 2>/dev/null | awk -F: '$3 == 0 {print $1}' 2>/dev/null)
+                        #search for sudo users
+                        Sudo1=$(awk -F: '($3 == "0") {print}' /etc/passwd 2>/dev/null
+                        Sudo2=$(grep -v -E "^#" /etc/passwd 2>/dev/null | awk -F: '$3 == 0 {print $1}' 2>/dev/null)
 
-                #print any sudo users found
-                echo "Sudo Users:"
-                for user in $(cut -d: -f1 /etc/passwd); 
-                do [ $(id -u $user) = 0 ] && echo $user; 
-                done | grep -v root
-
+                        #print any sudo users found
+                        echo "Sudo Users:"
+                        for user in $(cut -d: -f1 /etc/passwd); 
+                        do [ $(id -u $user) = 0 ] && echo $user; 
+                        done | grep -v root
+                fi
+                
                 #find other non-sudo users
                 echo "Looking for other users..."
                 $users=$(ls -ld /home/* 2>/dev/null)
@@ -89,24 +104,73 @@ until [ "$selection" = "0" ]; do
                 else
                         echo "No other users found, might need sudo privs to view"
                 fi
+                
+        #See what user can do
+        elif [ $selection == "2" ]; 
+                 #check if sudo can be used as the current user without a password
+                if [ 'echo "" | sudo -nS id"' && lse_sudo=true 2>/dev/null 1>/dev/null];
+                then 
+                        echo -en "Password not needed to run sudo with this user\n"
+                else
+                        echo -en "You must have a password to run sudo with current user\n"
+                fi
+
+                #check to see what all the user can run with sudo
+                if [ sudo -l 2>/dev/null 1>/dev/null ];
+                then
+                        echo -en "Commands $whoami can run with sudo"\n"
+                        sudo -l 2>/dev/null
+                else
+                        echo -en "Not able to view which sudo commands can be run\n"
+
+                
+                
+        #See Accesses
+        elif [ $selection == "3" ];
+                #See if root is able to log in using SSH
+                if [ grep -E "^[[:space:]]*PermitRootLogin " /etc/ssh/sshd_config | grep -E "(yes|without-password|prohibit-password)" 2>/dev/null 1>/dev/null ];
+                then
+                        echo "Root can log in using SSH"
+                else
+                        echo "Root is not able to log in using SSH"
+                fi
+
+                #See available shells
+                shells=$(cat /etc/shells)
+                echo "Available Shells:"
+                echo $shells
+           
+        elif [ $selection == "Q" || $selection == "q" ];
+        then
+                echo "Quitting";
+                break
         else
-                echo "Not a valid selection"
+                echo "Not a valid choice"
         fi
 done
 
-#See if root is able to log in using SSH
-if [ grep -E "^[[:space:]]*PermitRootLogin " /etc/ssh/sshd_config | grep -E "(yes|without-password|prohibit-password)" 2>/dev/null 1>/dev/null ];
-then
-        echo "Root can log in using SSH"
-else
-        echo "Root is not able to log in using SSH"
-fi
-
-#See available shells
-echo "Available Shells:"
-cat /etc/shells
-
 #_________________________________________________________________
-#                             FILES
+#                            TESTING
 #_________________________________________________________________
 
+'''
+C=$(printf '\033')
+Magenta="${C}[1;95m"
+LGray="${C}[1;37m"
+DGray="${C}[1;90m"
+Blue="${C}[1;34m"
+Cyan="${C}[1;96m"
+Red="${C}[1;31m"
+Normal="${C}[0m"
+
+printf """
+        ${Blue}What would you like to do?
+        __________________________
+        
+        ${Magenta}1. Find Users
+        ${Magenta}2. See what $user can do
+        ${Magenta}3. See Accesses
+        ${Red}0. Quit${Normal}
+        
+        """
+'''
