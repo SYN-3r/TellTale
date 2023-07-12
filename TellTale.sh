a67a344db8aecb3fe8a26b0bf6be4feef4c2e2fc
SYN-3r <135239201+SYN-3r@users.noreply.github.com>
2023-07-12 17:50:05 +0000

    Update TellTale.sh

--- TellTale.sh
+++ TellTale.sh
@@ -39,7 +39,7 @@ ${LGray}/-----------------------------------------------------------------------
 |${Red}::::::::::::::::::::::::::::::::::     ::::::::::::::::::::::::::::::::::${LGray}|
 |${Red}:::::::::::::::::::::::::::::::::::: ::::::::::::::::::::::::::::::::::::${LGray}|
 |${Red}:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::${LGray}|
-\-------------------------------------------------------------------------/
+\-------------------------------------------------------------------------/${Normal}
 
 """
 #Main Code
@@ -49,28 +49,34 @@ ${LGray}/-----------------------------------------------------------------------
 
 #basic start up info for log on
 #View what users are on the machine
-loggedon=$( w | awk -F" " '{ print $1,$4 }')
-echo "Users Currently on the machine:\n $loggedon"
-
+loggedon=$(w)
+printf """
+${Magenta} Users Currently on the machine:\n\n ${Normal} $loggedon \n
+"""
 #View which user you are
 user=$(whoami)
-echo "You are currently: $user"
+printf """
+\n 
+${Magenta} You are currently: ${Red} $user ${Normal} \n
+"""
 
 #MENU display start
 while true; 
 do
-        printf """
-                 ${Blue}MAIN MENU
-        What would you like to do?
-        __________________________
+printf """
+          ${Blue}MAIN MENU
+  What would you like to do?
+  __________________________
         
-        ${Magenta}1. Find Users
-        2. See what ${user} can do
-        3. View Files
-        4. See Accesses
-        ${Red}Q. Quit${Normal}
+  ${Magenta}1. Find Users
+  2. See what ${user} can do
+  3. See what root can do
+  4. View contents of /etc files
+  5. View cron abilities
+  6. See Accesses
+  ${Red}Q. Quit${Normal}
         
-        """
+"""
         read -r selection
 
         #Find Users
@@ -122,45 +128,103 @@ do
                         echo -en "Not able to view which sudo commands can be run\n"
                 fi
 
-
-        elif [ $selection == "3" ];
+        #view contents of /etc files
+        elif [ $selection == "4" ];
                 then
-                        declare -a dirs=("/etc/group" "/etc/passwd" "/etc/hosts" "/etc/shadow")
+                        declare -a dirs=("/etc/group" "/etc/passwd" "/etc/hosts" "/etc/shadow" "/etc/shells")
+                        declare -a gooddirs=()
                         for i in ${!dirs[@]};
                         do
                                 if [ $test -r ${dirs[$i]} ];
                                 then
                                         echo -en "${dirs[$i]}: YES\n"
+                                        gooddirs+=("${dirs[$i]}")
                                 else
                                         echo -en "${dirs[$i]}: NO\n"
                                 fi
                         done
+                        while true;
+                        do
+                                echo -en "Would you like to view contents?\n"
+                                read -r options
+                                if [ $options == "Y" ] || [ $options == "y" ];
+                                then
+                                while true;
+                                do
+                                        printf """
+${Blue}Which would you like to display?
+${Magenta}1. /etc/group
+2. /etc/passwd
+3. /etc/hosts
+4. /etc/shadow
+5. /etc/shells
+6. All
+${Red}Q. Quit${Normal}
+                                        """
+                                        read -r this
+                                        if [ $this == "1" ];
+                                        then
+                                                cat /etc/group
+                                                break
+                                        elif [ $this == "2" ];
+                                        then
+                                                cat /etc/passwd
+                                                break
+                                        elif [ $this == "3" ];
+                                        then
+                                                cat /etc/hosts
+                                                break
+                                        elif [ $this == "4" ];
+                                        then
+                                                cat /etc/shadow
+                                                break
+                                        elif [ $this == "5" ];
+                                        then
+                                                cat /etc/shells
+                                                break
+                                        elif [ $this == "6" ];
+                                        then
+                                                echo -en "Displaying all contents now...\n\n\n"
+                                                for a in ${!gooddirs[@]};
+                                                do
+                                                        cat ${gooddirs[$a]}
+                                                        echo -en "\n"
+                                                done
+                                                break
+                                        elif [ $this == "Q" ] || [ $this == "q" ];
+                                        then
+                                                break
+                                        else
+                                                echo "Please enter a valid option"
+                                        fi
+                                done
+                                elif [ $options == "N" ] || [ $options == "n" ];
+                                then
+                                        echo "Will not display contents"
+                                        break
+                                else
+                                        echo "Please enter a valid option"
+                                fi
+                        done
+                         
                         
         #See Accesses
-        elif [ $selection == "4" ];
-                #See if root is able to log in using SSH
-                if [ grep -E "^[[:space:]]*PermitRootLogin " /etc/ssh/sshd_config | grep -E "(yes|without-password|prohibit-password)" 2>/dev/null 1>/dev/null ];
-                then
-                        echo "Root can log in using SSH"
-                else
-                        echo "Root is not able to log in using SSH"
-                fi
-
-                #See available shells
-                shells=$(cat /etc/shells)
-                echo "Available Shells:"
-                echo $shells
-           
-        elif [ $selection == "Q" || $selection == "q" ];
+        elif [ $selection == "6" ];
+        then       
+                
+        #Quit   
+        elif [ $selection == "Q" ] || [ $selection == "q" ];
         then
                 echo "Quitting";
                 break
         else
-                echo "Not a valid choice"
+                echo "Please enter a valid choice"
         fi
 done
 
 #_________________________________________________________________
 #                            TESTING
 #_________________________________________________________________
+
+
 
